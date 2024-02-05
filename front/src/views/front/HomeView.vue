35a71ca2996b39f1d98b4a2372e484d8f03f1fb1
wenchicheng <wenchicheng@outlook.com>
2024-02-06 00:16:15 +0800

    swiper

--- front/src/views/front/HomeView.vue
+++ front/src/views/front/HomeView.vue
@@ -3,3 +3,20 @@
   <h1>HOME</h1>
 </v-container>
 </template>
+
+<script setup>
+// 引入 Swiper 核心和模塊
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
+
+// 引入 Swiper 样式
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+// init Swiper:
+// const swiper = new Swiper('.swiper', {
+//   modules: [Navigation, Pagination]
+// })
+
+</script>
