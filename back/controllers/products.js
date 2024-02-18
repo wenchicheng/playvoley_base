5bcd138dc52137aee2a4ba73c2a690bb33142fc0
wenchicheng <wenchicheng@outlook.com>
2024-02-19 00:28:49 +0800

    update

--- back/controllers/products.js
+++ back/controllers/products.js
@@ -79,7 +79,52 @@ export const getAll = async (req, res) => {
 
 // 前台用，只查有上架的商品=======================================================
 export const get = async (req, res) => {
+  try {
+    const sortBy = req.query.sortBy || 'createdAt'
+    // sortBy 是排序欄位，預設是 createdAt
+    const sortOrder = parseInt(req.query.sortOrder) || -1
+    // 資料排序，sortBy 是排序欄位，sortOrder 是排序方式（1 是升冪，-1 是降冪) ，parseInt() 是將字串轉換為數字
+    const itemsPerPage = parseInt(req.query.itemsPerPage) || 20
+    // itemsPerPage 是每頁顯示多少筆資料，預設是 20
+    const page = parseInt(req.query.page) || 1
+    // page 是目前在第幾頁，預設是第一頁
+    const regex = new RegExp(req.query.search || '', 'i')
+    // 查詢條件，關鍵字查詢 regex 是正則表達式，i 是忽略大小寫
 
+    // 查詢商品資料語法
+    // 1. 查詢 name 或 description 符合 regex 的資料
+    const data = await products
+      .find({
+        sell: true,
+        // sell: true 只查有上架的商品
+        $or: [
+          { name: regex },
+          { description: regex }
+        ]
+      })
+      // 2. 查詢完對結果作排序，[sortBy] => 不是陣列，是把變數的值當成物件的屬性名稱key
+      .sort({ [sortBy]: sortOrder })
+      // 3.跳過多少筆資料，如果1頁10筆，第1頁是 (1-1)*10，第2頁是 (2-1)*10 ，第3頁是 (3-1)*10，以此類推
+      .skip((page - 1) * itemsPerPage)
+      // 4. 限制一頁幾筆資料
+      .limit(itemsPerPage === -1 ? undefined : itemsPerPage)
+
+    // estimatedDocumentCount() 計算總資料數
+    const total = await products.estimatedDocumentCount()
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: '',
+      result: {
+        data, total
+      }
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: '未知錯誤'
+    })
+  }
 }
 
 // 取得單一商品=================================================================
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import * as auth from '../middlewares/auth.js'
-import { create, edit, getAll } from '../controllers/products.js'
+import { create, edit, getAll, get } from '../controllers/products.js'
 import upload from '../middlewares/upload.js'
 import admin from '../middlewares/admin.js'
 
@@ -12,5 +12,7 @@ router.get('/all', auth.jwt, admin, getAll)
 // 驗證有沒有登入jwt > 管理員驗證判斷權限 > 查詢所有商品
 router.patch('/:id', auth.jwt, admin, upload, edit)
 // 驗證有沒有登入jwt > 管理員驗證判斷權限 > 修改商品資料
+router.get('/', get)
+// 查詢單一商品(前台用)，不須登入，不須驗證權限
 
 export default router
